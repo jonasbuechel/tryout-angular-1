3a14d79b4916e89b5b7c87e69fbaae42dd80baf6
Jonas Büchel <jonas.buechel@gmail.com>
2016-03-21 22:24:59 +0100

    directive 'hello-world' :  rewrite code

--- scripts/hello-world.js
+++ scripts/hello-world.js
@@ -1,11 +1,9 @@
-'use strict';
-/*jslint browser: true*/
+/*jslint browser: true, devel: true, white: true, sloppy: true*/
 /*global angular */
 
-angular.module('todoListApp')
-    .directive('helloWorld', function() {
-        return {
-            template: 'this is my hello world directive :)',
-            restrict: 'E' //just allow integration as html element
-        };
-    });
+todoListApp.directive('helloWorld', function() {
+    return {
+        template: 'this is my hello world directive :)',
+        restrict: 'E' //just allow integration as html element
+    };
+});
