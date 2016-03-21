02d13049769eeffa72dbe3d2dd4cad75e442280f
Jonas Büchel <jonas.buechel@gmail.com>
2016-03-21 22:25:17 +0100

    app: add controller 'mainCtrl'

--- scripts/app.js
+++ scripts/app.js
@@ -1,4 +1,14 @@
-/*jslint browser: true*/
+/*jslint browser: true, devel: true, white: true, sloppy: true*/
 /*global angular */
 
-angular.module('todoListApp', []);
+/* create angular module */
+var todoListApp = angular.module('todoListApp', []);
+
+/* add a controller */
+todoListApp.controller('mainCtrl', function($scope) {
+    $scope.helloWorld = function() {
+        var element = document.createElement('span');
+        element.innerHTML = ' :) ';
+        console.log(document.querySelector('h1').appendChild(element));
+    };
+});
