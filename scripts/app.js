983623316c64078ddfe2e706fef5c8a7a21a182f
Jonas Büchel <jonas.buechel@gmail.com>
2016-03-22 06:27:31 +0100

    prototypal inheritance: added a parent and two different child controllers to simulate the prototypal inhertitance with console outputs

--- scripts/app.js
+++ scripts/app.js
@@ -6,9 +6,40 @@ var todoListApp = angular.module('todoListApp', []);
 
 /* add a controller */
 todoListApp.controller('mainCtrl', function($scope) {
+    /* FUNCTION: Hello World */
     $scope.helloWorld = function() {
-        var element = document.createElement('span');
-        element.innerHTML = ' :) ';
-        console.log(document.querySelector('h1').appendChild(element));
+        console.log('Hello  world, I\'m the main controller');
+    };
+    /* FUNCTION: whoAmI */
+    $scope.whoAmI = function() {
+        console.log('WhoAmI: I\'m the main controller');
+    };
+});
+
+/* add a controller */
+todoListApp.controller('parentCtrl', function($scope) {
+    /* FUNCTION: Hello World */
+    $scope.helloWorld = function() {
+        console.log('Parent: this child has no own helloWorld function, so I allow you to use mine');
+    };
+    /* FUNCTION: whoAmI */
+    $scope.whoAmI = function() {
+        console.log('Parent: this child has no own whoAmI function, so I allow you to use mine');
+    };
+});
+
+/* add a first children controller */
+todoListApp.controller('child1Ctrl', function($scope) {
+    /* FUNCTION: Hello World */
+    $scope.helloWorld = function() {
+        console.log('Hello  world, I\'m the child1 controller');
+    };
+});
+
+/* add a second children controller */
+todoListApp.controller('child2Ctrl', function($scope) {
+    /* FUNCTION: Hello World */
+    $scope.whoAmI = function() {
+        console.log('WhoAmI: I\'m the child2 controller');
     };
 });
