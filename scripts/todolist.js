3ddda2f99ea57d041fb01395b02be1a98a089390
Jonas Büchel <jonas.buechel@gmail.com>
2016-03-25 16:14:57 +0100

    todoList : added todolist as repeated html with functions per task

--- scripts/todolist.js
+++ scripts/todolist.js
@@ -7,19 +7,27 @@ angularPlaygroundApp.controller('todoCtrl', function($scope) {
     $scope.todolist = [];
     $scope.newTask = {};
 
+    //Add some testData
+    $scope.todolist.push({name: 'finish angular course'});
+    $scope.todolist.push({name: 'by milk', completed: true});
+
     $scope.saveTask = function() {
-        $scope.todolist.push(
-            {
-                name: $scope.newTask.name || null,
-                completed: $scope.newTask.state || false
-            }
-        );
+
+        if ($scope.newTask.name) {
+            $scope.todolist.push(
+                {
+                    name: $scope.newTask.name || null,
+                    completed: $scope.newTask.completed || false
+                }
+            );
+            $scope.newTask.name = null;
+        }
     };
 });
 
 /* add directive */
 angularPlaygroundApp.directive('todoList', function() {
-    
+
     return {
         template: 'saved values: {{todolist}}',
         restrict: 'E' //just allow integration as html element
